cd596a69afb4372765ade56d9dd3038a8710d2e9
marcel.aita <marcel.aita_silva@outlook.com>
2025-04-11 20:48:28 -0300

    BN-2

---
+++ Python/Enuciados/Lista_Exercicios/Exercicios/Lista 1.py
@@ -0,0 +1,17 @@
+#Crie um programa que imprima o seu nome completo,
+# sua idade e sua cidade em linhas separadas.
+
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+cep = input("Digite seu endereço: ")
+print ("nome\!n")
+print (idade"\n")
+print (cep) 
+
+#Crie um programa como o programa anterior, porém mostre todas
+# as informações em uma #única linha, separadas por vírgulas.
+
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+cep = input("Digite seu endereço: ")
+print (f"{nome}, {idade}, {cep}") 
