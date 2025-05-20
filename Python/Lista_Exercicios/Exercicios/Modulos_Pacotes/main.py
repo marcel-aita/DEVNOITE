4381f1bdfe7bb07df5ad2ac0130fe31c34c6c19f
marcel.aita <marcel.aita_silva@outlook.com>
2025-05-20 19:44:29 -0300

    Refactor imports in main.py to streamline module usage

--- Python/Lista_Exercicios/Exercicios/Modulos_Pacotes/main.py
+++ Python/Lista_Exercicios/Exercicios/Modulos_Pacotes/main.py
@@ -1,1 +1,2 @@
+from exercicios_Abstracoes import menu_Abs as mlab
 from operacoes import somar, diferenca
--- Python/Lista_Exercicios/Exercicios/main.py
+++ Python/Lista_Exercicios/Exercicios/main.py
@@ -1,4 +1,3 @@
 from exercicios_lista1 import menu_lista as ml
 from exercicios_OperadoresLogicos import menu_Op as mlop
 from exercicios_EstruturaAlgoritimo import menu_Alg as mla
-from exercicios_Abstracoes import menu_Abs as mlab
