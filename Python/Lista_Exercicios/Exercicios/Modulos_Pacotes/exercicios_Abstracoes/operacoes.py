039a1719d2fea529dad174953b9b922861b6ec01
marcel.aita <marcel.aita_silva@outlook.com>
2025-05-20 19:36:28 -0300

    Add operations module with sum function and update main import

---
+++ Python/Lista_Exercicios/Exercicios/Modulos_Pacotes/exercicios_Abstracoes/operacoes.py
@@ -0,0 +1,5 @@
+# Crie operacoes.py com soma(a, b) e diferenca(a, b).
+def somar(a, b):
+    return a + b
+def diferenca(a, b):
+    return a - b
---
+++ Python/Lista_Exercicios/Exercicios/Modulos_Pacotes/main.py
@@ -0,0 +1,1 @@
+from operacoes import somar, diferenca
