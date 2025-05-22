25b1c2983c901805bc8a9d2564f411cad48ecdcd
marcel.aita <marcel.aita_silva@outlook.com>
2025-05-21 21:21:28 -0300

    Refactor area.py to remove main function and clean up comments for area calculations

--- Python/Lista_Exercicios/Exercicios/Modulos_Pacotes/matematica/area.py
+++ Python/Lista_Exercicios/Exercicios/Modulos_Pacotes/matematica/area.py
@@ -1,12 +1,8 @@
-#calcule a área para um círculo de raio 5 e um quadrado de lado 4.
-# #Função area_circulo(raio) e area_quadrado(lado)
-from math import pi
+#Função area_circulo(raio)
 def area_circulo(raio):
     return pi * (raio ** 2)
+
+ #Função area_quadrado(lado)
 def area_quadrado(lado):
     return lado ** 2
-def main():
-    raio = 5
-    lado = 4
-    print(f"A área do círculo com raio {raio} é: {area_circulo(raio)}")
-    print(f"A área do quadrado com lado {lado} é: {area_quadrado(lado)}")
+
