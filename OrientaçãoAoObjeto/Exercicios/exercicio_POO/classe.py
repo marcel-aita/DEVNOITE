6eb7c7c0392f1921ad899582bc132bb4fc9b533c
marcel-aita <marcel.aita_silva@outlook.com>
2025-05-28 20:18:00 -0300

    Delete OrientaçãoAoObjeto/Exercicios/exercicio_POO directory

--- OrientaçãoAoObjeto/Exercicios/exercicio_POO/classe.py
+++
@@ -1,7 +0,0 @@
-class Pessoa:
-    def __init__(self, nome, idade):
-        self.nome = nome
-        self.idade = idade
-
-    def apresentar(self):
-        print(f"Olá, meu nome é {self.nome} e tenho {self.idade} anos.")
