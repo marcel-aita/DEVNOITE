fc47ce4729dfd7edad440b27308e5602b29e4a91
marcel.aita <marcel.aita_silva@outlook.com>
2025-06-02 20:21:58 -0300

    No code changes made

--- Orientação-AO-Objeto/Desenvolver Código Orientado a Objetos/Material/POO/global_imports.py
+++ Orientação-AO-Objeto/Desenvolver Código Orientado a Objetos/Material/POO/global_imports.py
@@ -1,3 +1,4 @@
+from POO.introducao.menu_introducao import menu_introducao
 from introducao.atributo import atributo_executar
 from introducao.classe import classe_executar
 from introducao.metodo import metodo_executar
