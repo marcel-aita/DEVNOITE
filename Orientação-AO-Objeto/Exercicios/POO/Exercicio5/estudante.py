431b1bd14cf806af60d5bd410f7abb19226a29c2
marcel.aita <marcel.aita_silva@outlook.com>
2025-06-05 20:20:51 -0300

    Add Estudante and Curso classes for exercise 5 functionality

---
+++ Orientação-AO-Objeto/Exercicios/POO/Exercicio5/estudante.py
@@ -0,0 +1,14 @@
+# Exercício 5 
+# 
+# Implemente um pacote com duas classes: 
+# uma representando um estudante com informações básicas 
+# e uma lista de notas, e outra representando um curso.
+# Inclua métodos para cálculo de média e para cadastrar estudantes no curso.
+
+class Estudante:
+    def __init__(self, nome, idade, notas)
+        self.nome = nome
+        self.idade = idade
+        self.notas= notas []
+
+        
