59f65f50cefb451a1690dc3b54ff2096953b68ff
marcel-aita <marcel.aita_silva@outlook.com>
2025-08-26 21:18:42 -0300

    Add files via upload

---
+++ PO_Marcia/Encapsulamento/JOptionPane/Main.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane; 
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String nome = JOptionPane.showInputDialog("Digite seu nome:");
+        String nota01Str = JOptionPane.showInputDialog("Digite sua nota:");
+        String nota02Str = JOptionPane.showInputDialog("Digite sua nota:");
+        String nota03Str = JOptionPane.showInputDialog("Digite sua nota:");
+        
+
+        
+
+
+
+    }
+}
